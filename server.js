c39709815d3ea85c3880f54bd9b306806ebaeabe
punitkhiyara98 <pkhiyara@terpmail.umd.edu>
2020-05-12 18:38:22 -0500

    Updates

--- server.js
+++ server.js
@@ -5,17 +5,10 @@
 // We're including a server-side version of Fetch to build on your client-side work
 import express from 'express';
 import fetch from 'node-fetch';
-
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-import writePermit from './libraries/writepermit';
-import filterPermits from './libraries/filter';
-const fsLibrary  = require('fs');
-
-const dbSettings = {
-  filename: './tmp/data.db',
-  driver: sqlite3.Database
-};
+const nodeGeocoder = require('node-geocoder');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
+require('dotenv/config');
 
 // Server Instantiation
 const app = express();
@@ -26,14 +19,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-async function dbBoot() {
-  const db = await open(dbSettings);
-  console.log('async DB boot');
-}
+// Connecting to DB
+
+
+const options = {
+  provider: 'here',
+  apiKey: process.env.API_KEY
+};
+ 
+const geoCoder = nodeGeocoder(options);
 
 async function startUp() {
+  await mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => console.log('Connected to DB!'));
 
-  let permits = [];
   await fetch('https://data.princegeorgescountymd.gov/resource/weik-ttee.json')
     .then((results) => results.json())
     .then((data) => {
@@ -52,55 +50,91 @@ async function startUp() {
             zip: element.zip_code,
             date: element.permit_issuance_date,
             cost: element.expected_construction_cost,
-            full_location: element.location
+            fullLocation: element.location
           };
           refined.push(obj);
         }
       });
-      console.log(refined);
+      console.log('Permit Records: ', refined.length);
       return refined;
     })
-    .then((data) => {
-      permits = data;
+    .then(async (data) => {
+      await data.forEach(async (element) => {
+        let latitude;
+        let longitude;
+        if (element.fullLocation) {
+          await geoCoder.geocode(element.fullLocation)
+            .then(async (res) => {
+              if (!res[0]) {
+                latitude = 'N/A';
+                longitude = 'N/A';
+              } else {
+                latitude = 'N/A';
+                longitude = 'N/A';
+                if (res[0]['county'] === "Prince George's") {
+                  latitude = res[0]['latitude'];
+                  longitude = res[0]['longitude'];
+                }
+              }
+            })
+            .catch((err) => {
+              if (err.code !== 'ECONNRESET' && err.code !== 'ENOTFOUND' && err.code !== 'ETIMEDOUT') {
+                console.log(err);
+              }
+            });
+        }
+        // Writing to DB
+        const post = new Post({
+          id: element.id,
+          category: element.category,
+          agency: element.agency,
+          year: element.year,
+          type: element.type,
+          name: element.name,
+          address: element.address,
+          city: element.city,
+          zip: element.zip,
+          date: element.date,
+          cost: element.cost,
+          fullLocation: element.fullLocation,
+          lat: latitude,
+          long: longitude
+        });
+        try {
+          const savedPost = await post.save();
+        } catch (err) {
+          if (err.code !== 11000) {
+            console.log(err);
+          }
+        }
+      });
     })
     .catch((err) => {
+      console.log('Fetch Error');
     });
-
-  await console.log(permits);
-  await permits.forEach((element) => { 
-    fetch('https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address='+element.full_location+'&benchmark=Public_AR_Current&format=json')
-      .then((results) => results.json())
-      .then((dt) => {
-        if (!dt["result"]["addressMatches"]) {
-          writePermit(element.id, element.category, element.agency, element.year, element.type, element.name, element.address, element.city, element.zip, element.date, element.cost, element.full_location, "N/A", "N/A", dbSettings);
-        } else {
-          writePermit(element.id, element.category, element.agency, element.year, element.type, element.name, element.address, element.city, element.zip, element.date, element.cost, element.full_location, dt["result"]["addressMatches"][0]["coordinates"]["y"], dt["result"]["addressMatches"][0]["coordinates"]["x"], dbSettings);
-        }
-      })
-      .catch((error) => {
-      });
-  });
 }
 
+startUp();
+
 async function processDataForFrontEnd(req, res) {
-  await startUp();
-  const lowerrange = req.body.year.slice(1, 5);
-  const upperrange = req.body.year.slice(8, 12);
-  const daterange = [lowerrange, upperrange];
-  const lowercost = req.body.amount.split('-')[0].trim().slice(1);
-  const uppercost = req.body.amount.split('-')[1].trim().slice(1);
-  const pricerange = [lowercost, uppercost];
-  const results = filterPermits(lowerrange, upperrange, lowercost, uppercost, dbSettings);
-  console.log(results);
-  async function findPermit(id, dbSettings) {
-    const db = await open(dbSettings);
-    const dat = await db.get('SELECT * FROM permits WHERE id = ?', [id], (err, row) => {
-      if (err) {
-        throw err;
-      }
-      return row;
-    });
-  }
+  // startUp();
+  // const lowerrange = req.body.year.slice(1, 5);
+  // const upperrange = req.body.year.slice(8, 12);
+  // const daterange = [lowerrange, upperrange];
+  // const lowercost = req.body.amount.split('-')[0].trim().slice(1);
+  // const uppercost = req.body.amount.split('-')[1].trim().slice(1);
+  // const pricerange = [lowercost, uppercost];
+  // const results = filterPermits(lowerrange, upperrange, lowercost, uppercost, dbSettings);
+  // console.log(results);
+  // async function findPermit(id, dbSettings) {
+  //   const db = await open(dbSettings);
+  //   const dat = await db.get('SELECT * FROM permits WHERE id = ?', [id], (err, row) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     return row;
+  //   });
+  // }
 
 }
 
@@ -109,28 +143,14 @@ async function processDataForFrontEnd(req, res) {
 // by typing in: localhost:3000/api or 127.0.0.1:3000/api
 app
   .route('/api')
-  .get((req, res) => {
-    (async () => {
-      const db = await open(dbSettings);
-      const result = await db.all('SELECT * FROM permits');
-      res.json(result);
-    })();
+  .get(async (req, res) => {
+    try {
+      const posts = await Post.find();
+      res.json(posts);
+    } catch (err) {
+      console.log(err);
+    }
   })
   .post((req, res) => { processDataForFrontEnd(req, res); });
-
-// app
-//   .route('/api')
-//   .get((req, res) => {
-//     (async () => {
-//       const db = await open(dbSettings);
-//       const result = await db.all('SELECT * FROM user');
-//       console.log('Expected result', result);
-//       res.json(result);
-//     })();
-//   })
-//   .post((req, res) => {
-//     
-//     }
-//   });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
